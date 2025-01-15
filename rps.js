1ba3f44af3595924fcddc33002c860067fdc370f
Nevin Hill <nhill.mail@gmail.com>
2025-01-15 14:38:23 -0500

    Add getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,2 +1,10 @@
 // Test that "rps.js" file is linked to "index.html" file.
-console.log('Hello World!'); //success: printed to console.
+console.log('Hello World!'); //success: printed to console
+
+// logic to get the computer choice of a random number between 0 an 3
+function getComputerChoice(max){
+    return Math.floor(Math.random() * max);
+
+}
+    // check if getComputerChoice function is working in console
+    console.log(getComputerChoice(3));//success
