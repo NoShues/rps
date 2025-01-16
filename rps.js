c862f83abf09d6c40e77a8c3b9b42e3b9cf4a527
Nevin Hill <nhill.mail@gmail.com>
2025-01-16 10:24:10 -0500

    Remove project instructions

--- rps.js
+++ rps.js
@@ -1,12 +1,3 @@
-// Write the logic to get the computer choice
-// Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
-// Create a new function named getComputerChoice.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
-// Note: Some learners might think that arrays are necessary for this problem, but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
-// Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
-
-// *****************************************************************************************************//
     // SEQUENCE computer choice
         // COMPUTE a random number between 0 and 2 to be the computer choice
             // SET computer choice
@@ -17,15 +8,6 @@
                         // THEN Display: 'Paper'
                     // ELSE 
                         // Display: 'Scissors'
-// *****************************************************************************************************//
-
-// Write the logic to get the human choice
-// Your game will be played by a human player. You will write a function that takes the user choice and returns it.
-// Create a new function named getHumanChoice.
-// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
-// Hint: Use the prompt method to get the user’s input.
-// Test what your function returns by using console.log.
-
 // *****************************************************************************************************//
     // SEQUENCE human choice
         // GET human choice
@@ -42,28 +24,11 @@
                             // Display: 'Please choose '"Rock", "Paper" or "Scissors"'
                             // GET new human choice
 // *****************************************************************************************************//
-
-// Declare the players score variables
-// Your game will keep track of the players score. You will write variables to keep track of the players score.
-// Create two new variables named humanScore and computerScore in the global scope.
-// Initialize those variables with the value of 0.
-
-// *****************************************************************************************************//
     // SEQUENCE declare scores
         // SET human score
             // INIT human score to a value of 0
         // SET computer score
             // INIT computer score to a value of 0
-// *****************************************************************************************************//
-
-// Write the logic to play a single round
-// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-// Create a new function named playRound.
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-// Increment the humanScore or computerScore variable based on the round winner.
-
 // *****************************************************************************************************//
     // SEQUENCE play round
         // GET human choice and computer choice
@@ -90,17 +55,6 @@
             // ELSE
                 // Display: 'Oh, oh! Looks like something went wrong'
 // *****************************************************************************************************//
-
-// Write the logic to play the entire game
-// Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-// Create a new function named playGame.
-// Move your playRound function and score variables so that they’re declared inside of the new playGame function
-// Play 5 rounds by calling playRound 5 times.
-// Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-// Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-// If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
-
-// *****************************************************************************************************//
     // SEQUENCE play game
         // CALL play round 5 times
             // IF human score is greater than computer score
@@ -111,5 +65,4 @@
                 // THEN Display: 'Unbelievable! It was a draw!'
             // ELSE
                 // Display: 'Uh, oh! Something went wrong'
-
 // *****************************************************************************************************//
