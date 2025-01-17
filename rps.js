3eb5d5689bef371730e8cbc27a5e0b6908042723
Nevin Hill <nhill.mail@gmail.com>
2025-01-17 14:33:39 -0500

    Add loop to playGame function & remove comments.
    The previous option was not efficient.  Looping the
    playGame fx is more efficient and easier to read.

--- rps.js
+++ rps.js
@@ -1,14 +1,3 @@
-    // SEQUENCE computer choice
-        // COMPUTE random number between 0 and 3 to be the computer choice
-            // SET computer choice
-                // IF-THEN-ELSE
-                    // IF computer choice is equal to 0
-                        // THEN Display: 'Rock'
-                    // IF computer choice is equal to 1
-                        // THEN Display: 'Paper'
-                    // ELSE 
-                        // Display: 'Scissors'
-// *****************************************************************************************************//
 function getComputerChoice(max){
     let computerChoice = Math.floor(Math.random() * max);
     if (computerChoice == 0){
@@ -19,23 +8,7 @@ function getComputerChoice(max){
         computerChoice = 'Scissors';
         return computerChoice
 }
-// console.log('getComputerChoice fx: ' + getComputerChoice(3));
-// *****************************************************************************************************//
-    // SEQUENCE human choice
-        // GET human choice
-            // SET human choice to capitalize first letter ONLY
-                // DETERMINE if the human choice is a valid choice
-                    // IF-THEN-ELSE
-                        // IF human choice is equal to 'Rock'
-                            // THEN Display: 'Rock'
-                        // IF human choice is equal to 'Paper' 
-                            // THEN Display: 'Paper'
-                        // IF human choice is equal to 'Scissors'
-                            // THEN Display: 'Scissors'
-                        // ELSE
-                            // Display: 'Please choose '"Rock", "Paper" or "Scissors"'
-                            // GET new human choice
-// *****************************************************************************************************//
+
 function getHumanChoice(){
     const userValue = (prompt('Enter: "Rock", "Paper" or "Scissors"'));
     let humanChoice = userValue.toUpperCase().charAt(0) + userValue.toLowerCase().slice(1,8);
@@ -49,44 +22,10 @@ function getHumanChoice(){
         humanChoice = alert(`No ${userValue}'s allowed!`) + getHumanChoice(); 
     return String(humanChoice)
 }
-// console.log('getHumanChoice fx: ' + getHumanChoice());
-// *****************************************************************************************************//
-    // SEQUENCE declare scores
-        // SET human score
-            // INIT human score to a value of 0
-        // SET computer score
-            // INIT computer score to a value of 0
-// *****************************************************************************************************//
+
 let humanInitialScore = 0;
 let computerInitialScore = 0;
-// *****************************************************************************************************//
-    // SEQUENCE play round
-        // GET human choice and computer choice
-            // IF human choice is equal to computer choice
-                // THEN Display: 'It's a Draw!'
-            // IF human choice is equal to 'Rock' and computer choice is equal to 'Scissors'
-                // THEN Display: 'You win! Rock beats Scissors'
-                // INCREMENT human score by 1
-            // IF human choice is equal to 'Paper' and computer choice is equal to 'Rock'
-                // THEN Display: 'You Win! Paper beats Rock'
-                // INCREMENT human score by 1
-            // IF human choice equals 'Scissors' and computer choice equals 'Paper'
-                // THEN Display: 'You win! Scissors beats paper'
-                // INCREMENT human score by 1
-            // IF human choice equals 'Rock' and computer choice equals 'Paper'
-                // THEN Display: 'You lose! Paper beats Rock'
-                // INCREMENT computer score by 1
-            // IF human choice equals 'Paper' and computer choice equals 'Scissors'
-                // THEN Display: 'You lose! Scissors beats Paper'
-                // INCREMENT computer score by 1
-            // IF human choice equals 'Scissors' and computer choice equals 'Rock'
-                // THEN Display: 'You lose! Rock beats Paper'
-                // INCREMENT computer score by 1
-            // ELSE
-                // Display: 'Oh, oh! Looks like something went wrong'
 
-playGame();
-// *****************************************************************************************************//
 function playRound (){
     const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice(3);
@@ -108,36 +47,12 @@ function playRound (){
         result = 'Oh, oh! Looks like something went wrong!'
         return result 
 }
-// *****************************************************************************************************//
-    // SEQUENCE play game
-        // CALL play round 5 times
-            // IF human score is greater than computer score
-                // THEN Display: 'Congratulations: YOU WIN!'
-            // IF human score is less than computer score
-                // THEN Display: 'Better luck next time: YOU LOSE'
-            // IF human score is equal to computer score
-                // THEN Display: 'Unbelievable! It was a draw!'
-            // ELSE
-                // Display: 'Uh, oh! Something went wrong'
 
-// *****************************************************************************************************//
 function playGame(){
-
+for (let i =0; i < 5; i++){
     playRound()
     console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
-    console.log(result);
-    playRound()
-    console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
-    console.log(result);
-    playRound()
-    console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
-    console.log(result);
-    playRound()
-    console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
-    console.log(result);
-    playRound()
-    console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
-    console.log(result);
+    console.log(result);}
     if (humanInitialScore > computerInitialScore){
         message = 'Congratulations: YOU WIN!'
     } else if (humanInitialScore < computerInitialScore){
@@ -147,8 +62,8 @@ function playGame(){
     } else
     message = 'Uh, oh! Something went wrong'
 
-    return message
+    return message;
     
 }
-// *****************************************************************************************************//
+playGame();
 console.log(message)
