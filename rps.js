1982b1aa9f78d6d26c1475a9b6176a87a7225765
Nevin Hill <nhill.mail@gmail.com>
2025-01-24 07:45:29 -0500

    Add code for user to select their choice by button

--- rps.js
+++ rps.js
@@ -1,3 +1,15 @@
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+ playRound(button.id);
+  });
+});
+
+
 function getComputerChoice(max){
     let computerChoice = Math.floor(Math.random() * max);
     if (computerChoice == 0){
@@ -9,49 +21,37 @@ function getComputerChoice(max){
         return computerChoice 
     }
     
-    function getHumanChoice(){
-        const userValue = (prompt('Enter: "Rock", "Paper" or "Scissors"'));
-        let humanChoice = userValue.toUpperCase().charAt(0) + userValue.toLowerCase().slice(1,8);
-        if (humanChoice == 'Rock'){
-            humanChoice = 'Rock'
-        } else if (humanChoice == 'Paper'){
-            humanChoice = 'Paper'
-        } else if(humanChoice == 'Scissors'){
-            humanChoice = 'Scissors'
-        } else
-        humanChoice = alert(`No ${userValue}'s allowed!`) + getHumanChoice(); 
-        return String(humanChoice)
-    }
     
     let humanInitialScore = 0;
     let computerInitialScore = 0;
     
-    function playRound (){
-        const humanChoice = getHumanChoice();
+    function playRound (humanChoice){
         const computerChoice = getComputerChoice(3);
         if (humanChoice == computerChoice){
             result = 'It is a draw!'
         } else if (humanChoice == 'Rock' && computerChoice == 'Scissors'){
-        result = 'You Win! Rock beats Scissors', ++humanInitialScore
-    } else if (humanChoice == 'Paper' && computerChoice == 'Rock'){
-        result = 'You Win! Paper beats Rock', ++humanInitialScore
-    } else if (humanChoice == 'Scissors' && computerChoice == 'Paper'){
-        result = 'You Win! Scissors beats Paper', ++humanInitialScore
-    } else if (humanChoice == 'Rock' && computerChoice == 'Paper'){
-        result = 'You lose! Paper beats Rock', ++computerInitialScore
-    } else if (humanChoice == 'Paper' && computerChoice == 'Scissors'){
-        result = 'You lose! Scissors beats Paper', ++computerInitialScore
-    } else if (humanChoice == 'Scissors' && computerChoice == 'Rock'){
-        result = 'You lose! Rock beats Scissors', ++computerInitialScore
-    } else
-    result = 'Oh, oh! Looks like something went wrong!'
-    return result 
-}
+            result = 'You Win! Rock beats Scissors', ++humanInitialScore
+        } else if (humanChoice == 'Paper' && computerChoice == 'Rock'){
+            result = 'You Win! Paper beats Rock', ++humanInitialScore
+        } else if (humanChoice == 'Scissors' && computerChoice == 'Paper'){
+            result = 'You Win! Scissors beats Paper', ++humanInitialScore
+        } else if (humanChoice == 'Rock' && computerChoice == 'Paper'){
+            result = 'You lose! Paper beats Rock', ++computerInitialScore
+        } else if (humanChoice == 'Paper' && computerChoice == 'Scissors'){
+            result = 'You lose! Scissors beats Paper', ++computerInitialScore
+        } else if (humanChoice == 'Scissors' && computerChoice == 'Rock'){
+            result = 'You lose! Rock beats Scissors', ++computerInitialScore
+        } else
+        result = 'Oh, oh! Looks like something went wrong!'
+        return result, console.log(result), console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);
+    }
+    console.log('Computer Choice: ' + getComputerChoice());
 
 // function playGame(){
 //     for (let i =0; i < 5; i++){
 //         playRound()
-//            console.log(result);
+//         console.log('Computer Choice: ' + getComputerChoice());
+//         console.log(result);
 //         console.log('Human Score: ' + humanInitialScore + ' Computer Score: ' + computerInitialScore);}
 //     if (humanInitialScore > computerInitialScore){
 //         message = '🎉Congratulations🎉 🏆 YOU WIN! 🏆'
